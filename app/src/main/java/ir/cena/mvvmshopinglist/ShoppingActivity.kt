4346bf56c7511c49192688a226a1a7723d7181e9
cena tabriziyan <cena.tabriziyan@gmail.com>
2020-04-11 20:38:22 +0430

    create activity for shopping

---
+++ app/src/main/java/ir/cena/mvvmshopinglist/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package ir.cena.mvvmshopinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
